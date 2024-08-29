7d530fd0e1f4f167336f6e00093f0a83ad0c31cf
Simon Kristiansson <simonkristiansson91@gmail.com>
2024-08-29 11:43:31 +0200

    bragwall id

--- resources/js/Partials/Bragwall.vue
+++ resources/js/Partials/Bragwall.vue
@@ -28,7 +28,7 @@ onMounted(() => {
 
 <template>
     <div class="py-12 pt-0">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div id="bragwall" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div class=" text-xl text-gray-900">Brag wall</div>
                 <p class="mb-2"></p>
